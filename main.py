8638387416a6ef7a8ac0689fc77ccb9573884c3a
NoahjmHolt <noahjmholt@gmail.com>
2026-02-10 16:50:40 -0700

    Seperate files
    Think there will be more data extraction so made new file and imported in main to collect those functions

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+from stats import *
+
 
 def get_book_text(filepath):
 
     with open(filepath) as f:
         file = f.read()
         return file
-
-def seperate_leafs(whole_book):
-
-    seperate = whole_book.split()
-    return len(seperate)
 
 def main():
 
---
+++ stats.py
@@ -0,0 +1,4 @@
+def seperate_leafs(whole_book):
+
+    seperate = whole_book.split()
+    return len(seperate)
