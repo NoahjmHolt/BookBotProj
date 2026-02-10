221504b750b1f6aab8ced540b63a16d92c3b61e9
NoahjmHolt <noahjmholt@gmail.com>
2026-02-10 16:47:39 -0700

    Began main
    Began main by experimenting with a few functions to manipulate the files containing the books and seeing the types of data we can pull from it.

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 
-print('greetings boots')
+def get_book_text(filepath):
+
+    with open(filepath) as f:
+        file = f.read()
+        return file
+
+def seperate_leafs(whole_book):
+
+    seperate = whole_book.split()
+    return len(seperate)
+
+def main():
+
+    pages = get_book_text('./books/frankenstein.txt')
+    count_words = seperate_leafs(pages)
+
+    print(f'Found {count_words} total words')
+
+
+main()
