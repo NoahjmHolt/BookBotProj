28b8a1b8b3b0b75c49f6193f3bf4e5b473de31cc
NoahjmHolt <noahjmholt@gmail.com>
2026-03-01 14:10:07 -0700

    quick fix
    quick fix to get only alpha characters in output

--- stats.py
+++ stats.py
@@ -9,7 +9,7 @@ def char_counter(whole_book):
 
     for char in whole_book:
         lowered = char.lower()
-        if lowered != ' ':
+        if lowered.isalpha():
             char_tracker[lowered] = char_tracker.get(lowered, 0) + 1
 
     return char_tracker
